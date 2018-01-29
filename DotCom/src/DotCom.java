f5fdb1be602b6e6d168e840d2a61d37f57481d22
Nyolczas <nyolczas@gmail.com>
2018-01-29 21:39:13 +0100

    DotCom

--- DotCom/src/DotCom.java
+++ DotCom/src/DotCom.java
@@ -15,6 +15,17 @@ public class DotCom {
 	public String checkYourself(String userInput) {
 		String result = "miss";
 		
+		int index = locationCells.indexOf(userInput);
+		if(index >= 0) {
+			locationCells.remove(index);
+			
+			if (locationCells.isEmpty()) {
+				result = "kill";
+				System.out.println("Ouch! You sunk " + name + " : (");
+			} else {
+				result = "hit";
+			}
+		}
 		return result;
 	}
 }
