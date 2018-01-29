28538ae3c81506854e56455b081d12374ea0bb40
Nyolczas <nyolczas@gmail.com>
2018-01-29 22:22:01 +0100

    DotCom

--- DotCom/src/GameHelper.java
+++ DotCom/src/GameHelper.java
@@ -2,6 +2,13 @@ import java.io.*;
 import java.util.*;
 
 public class GameHelper {
+	
+	private static final String alphabet = "abcdefg";
+	private int gridLength = 7;
+	private int gridSize = 49;
+	private int[] grid = new int[gridSize];
+	private int comCount = 0;
+	
 	public String getUserInput(String prompt) {
 		String inputLine = null;
 		System.out.print(prompt + " ");
@@ -13,6 +20,37 @@ public class GameHelper {
 		} catch (IOException e) {
 			System.out.println("IOException: " + e);
 		}
-		return inputLine;
+		return inputLine.toLowerCase();
+	}
+	
+	public ArrayList<String> placeDotCom(int comSize) {
+		ArrayList<String> alphaCells = new ArrayList<String>();
+		String [] alphacoords = new String [comSize];
+		String temp = null;
+		int [] coords = new int[ comSize];
+		int attempts = 0;
+		boolean success = false;
+		int location = 0;
+		
+		comCount++;
+		int incr = 1;
+		if ((comCount % 2) == 1) {
+			incr = gridLength;
+		}
+		
+		while ( !success & attempts++ < 200) {
+			location = (int) (Math.random() * gridSize);
+			int x = 0;
+			success = true;
+			while (success && x < comSize) {
+				if(grid[ location] == 0) {
+					coords[ x++] = location;
+					location += incr;
+					if (location >= gridSize) {
+						success = false;
+					}
+				}
+			}
+		}
 	}
 }
