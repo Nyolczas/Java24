a24957087bc18592382674a4a81bb2e8559e18d1
Nyolczas <nyolczas@gmail.com>
2018-03-17 19:23:08 +0100

    myDrawPanel

---
+++ Swing/src/MyDrawPanel.java
@@ -0,0 +1,18 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+class MyDrawPanel extends JPanel {
+    MyDrawPanel() {
+    }
+
+    public void paintComponent(Graphics g) {
+        g.fillRect(0, 0, this.getWidth(), this.getHeight());
+        int red = (int)(Math.random() * 255.0D);
+        int green = (int)(Math.random() * 255.0D);
+        int blue = (int)(Math.random() * 255.0D);
+        Color randomColor = new Color(red, green, blue);
+        g.setColor(randomColor);
+        g.fillOval(70, 70, 100, 100);
+    }
+}
